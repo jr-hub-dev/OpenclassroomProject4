c9c0a2133309ab3516237c7cf0447c4bfff4a5a6
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-06-25 17:05:01 +0200

    maj controller post controller

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Controller\Controller;
+use App\Controller\PostController;
+
 
 //spl_autoload_register
 //routeur - si index.php tel conttroller si autre autre controller
@@ -9,15 +11,35 @@ use App\Controller\Controller;
         //appel de la vue -> methode GET
         //renvoi vue + objet
 
-require '../Autoloader/Autoloader.php';
-Autoloader::register();
+
+spl_autoload_register(function ($class) {
+    $class = '../' . str_replace("\\", '/', $class) . '.php';
+    $class = str_replace("App/", '', $class);
+
+    if (is_file($class)) {
+    	require_once($class);
+    } else {
+    //throw new CustomException('Erreur interne de chargement');
+    	var_dump($class);
+        
+	}
+});
 
 //Page d'accueil
-if ('home' === $_GET['action']   || $_GET['action'] === '') {
-        $controller = new Controller;
-        $controller->home();
+if ('home' === $_GET['action'] || '' === $_GET['action']) {
+    $controller = new Controller; 
+    $controller->home();
 //Page Post
-} elseif ($_GET['action'] === 'post') {
-        $postController = new PostController;
-        $postController->view($id);
+} elseif ('post' === $_GET['objet']) {
+    $postController = new PostController;
+    if ('view' === $_GET['action']) {        
+        $postController->view($_GET['id']);
+    //Creation du post
+    } elseif ('create' === $_GET['action']) {// objet ) post action =
+        $postController->create();
+    //Modification du post
+    } elseif ('modify' === $_GET['action']) {// objet ) post action =
+        $postController->modify();
+        //if action = create; if action 
+    }
 }
