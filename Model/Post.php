2cd922c220b73ea50fbe7c690b68d86ba22dff3b
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-06-25 17:10:10 +0200

    ajoute Model

---
+++ Model/Post.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Model;
+
+use DateTime;
+
+class Post
+{
+    private $id;
+    private $title;
+    private $creationDate;
+    private $updateDate;
+
+
+    public function getId() : ?int //a utilisre dans la vue
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id) : self //int ou nul
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    
+    public function getTitle() : ?string //a utilisre dans la vue
+    {
+        return $this->title;
+    }
+
+
+    public function setTitle(?string $title) : self //string ou nul
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent() : ?string //a utilisre dans la vue
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content) : self //int ou nul
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+    public function getCreationDate() : ?DateTime //a utilisre dans la vue
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(?DateTime $creationDate)// : self //int ou nul
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+    public function getUpdateDate() : ?DateTime //a utilisre dans la vue
+    {
+        return $this->updateDate;
+    }
+
+    public function setupdateDate(?DateTime $updateDate)// : self //int ou nul
+    {
+        $this->updateDate = $updateDate;
+
+        return $this;
+    }
+}
---
+++ Model/PostManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\Database;
+use DateTime;
+
+class PostManager extends Database { 
+
+    public function getPost($postId) 
+    {   
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('SELECT id, title, content, creationDate, updateDate FROM post WHERE id = ?'); // formater la date dans la vue + table avec 5 champs
+        $req->execute(array($postId));
+
+        return $this->hydrate($req->fetch());
+    }
+
+    public function getPosts() 
+    {   
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('SELECT id, title, content, creationDate, updateDate FROM post'); // formater la date dans la vue + table avec 5 champs
+        $req->execute();
+        $result = $req->fetchAll();
+        var_dump($result);
+
+        $posts = [];
+        foreach ($result as $post) {
+            //var_dump($post);
+            $posts[] = $this->hydrate($post);
+        }
+
+        return $posts;
+    }
+
+    public function create($title, $content)
+    {
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('INSERT INTO post(title, content, creationDate, updateDate) VALUES (?, ?, NOW(), NOW())');
+        $req->execute(array($title, $content));
+
+        return $bdd->lastInsertId();
+    }
+    public function modify($title, $content)
+    {
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('INSERT INTO post(title, content, creationDate, updateDate) VALUES (?, ?, NOW(), NOW())');//a voir
+        $req->execute(array($title, $content));
+
+        return $bdd->lastInsertId();//id du post a recuprer
+    }
+
+    public function deletePost($postId)
+    {
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('DELETE FROM post WHERE id = ?');
+        $deletedPost = $req->execute(array($postId));
+
+        return $deletedPost;
+    }
+
+    public function hydrate($data)
+    {
+        /*var_dump($data);
+        var_dump(new DateTime());
+        var_dump(new DateTime($data['creationDate']));*/
+        $post = new Post();
+        $post
+            ->setId($data['id'])
+            ->setTitle($data['title']) 
+            ->setContent($data['content'])
+            ->setCreationDate(new DateTime($data['creationDate']))
+            ->setUpdateDate(new DateTime($data['updateDate']))
+        ;
+        //var_dump($post);
+        return $post;
+    }
+}
