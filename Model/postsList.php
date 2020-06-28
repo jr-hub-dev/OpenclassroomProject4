3debeb80bc5d63fc7d108b8767e22844a2169ffb
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-06-28 14:54:56 +0200

    maj postsList

--- Model/postsList.php
+++ Model/postsList.php
@@ -11,6 +11,8 @@
             <td><?php echo $post->getTitle(); ?></td>
             <td><?php echo $post->getContent(); ?></td>
             <td><?php echo $post->getId(); ?></td>
+            <td><input type="button" method='modify' id="modifie-site"value="Modifier" /></td>
+            <td><input type="button" method='delete' id="delete-site" value="Supprimer" /></td>
         </tr>
         <?php } ?>
     </body>
