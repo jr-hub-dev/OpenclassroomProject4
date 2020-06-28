d9e0bd15152b4d3d5187c8706e461a562b6d9f6b
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-06-28 12:06:30 +0200

    ajout du header dans la view

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<header> 
+	<nav id="banniere">
+
+		<ul id="menu">
+			<li><a href=>Accueil</a></li>
+			<li><a href=>A proros de l'auteur</a></li>						
+			<li><a href=>Chapitres</a></li>
+			<li><a href=>Contact</a></li>
+		</ul>
+		<form id="bookingForm" > 
+			<label for="name">Nom : </label><input type="text" id="name" placeholder="Votre nom" required/><br />
+			<label for="firstname"> Prénom : </label><input type="text" id="firstname" placeholder="Votre prénom" required><br />
+
+			<input type="submit" name="submit" id="connexion" value="Se connecter">
+		</form>
+	</nav>
+</header>
