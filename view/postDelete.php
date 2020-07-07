a203429cbda51a6cba961ae44e1984e55881fc64
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-07-07 18:23:28 +0200

    modif nom de fichier postForm

---
+++ view/postDelete.php
@@ -0,0 +1,9 @@
+<form method="post">
+    <label for='newBilletTitle'>Titre du chapitre</label>
+    <input type='text' name='title' id='newBilletTitle' value="<?php if (isset($post)) echo $post->getTitle(); ?>" disabled="disabled">
+
+    <label for='newBilletTitle'>Contenu</label>
+    <textarea name='content' id='newBilletTitle' disabled="disabled"><?php if (isset($post)) echo $post->getContent(); ?></textarea>
+    <input type="hidden" name="delete" value="delete" />
+    <input type="submit" value="Valider" />
+</form>
