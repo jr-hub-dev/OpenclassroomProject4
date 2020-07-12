1b285a1c08a44e9e5790255b3fc6e688f406fe6b
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-07-12 19:43:02 +0200

    maj

--- view/header.php
+++ view/header.php
@@ -6,8 +6,9 @@
 			<li><a href=>A proros de l'auteur</a></li>						
 			<li><a href= 'index.php?objet=post&action=postsList'>Chapitres</a></li>
 			<li><a href=>Contact</a></li>
+			<li><a href= 'index.php?objet=user&action=create'>S'enregistrer</a></li>
 		</ul>
-		<form id="bookingForm" > 
+		<form method="post" > 
 			<label for="name">Login : </label><input type="text" id="name" placeholder="Votre nom" required/><br />
 			<label for="firstname"> Mot de passe : </label><input type="text" id="firstname" placeholder="Votre prénom" required><br />
 			<input type="submit" name="submit" id="connexion" value="Se connecter">
