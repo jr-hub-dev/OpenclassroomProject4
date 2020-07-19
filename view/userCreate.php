2f0f4cf5cef308066ff6614551c5c9e1e9ed885b
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-07-19 16:07:14 +0200

    maj userCreate

--- view/userCreate.php
+++ view/userCreate.php
@@ -1,11 +1,11 @@
 <form method="post">
-    <label for="newUser">Titre du chapitre</label>
+    <label for="newUser">Login</label>
     <input type="text" name="userLogin" id="userLogin">
 
-    <label for="newUser">Titre du chapitre</label>
+    <label for="newUser">Mot de passe</label>
     <input type="text" name="userPassword" id="userPassword">
 
-    <label for="newUser">Titre du chapitre</label>
+    <label for="newUser">Email</label>
     <input type="text" name="userEmail" id="userEmail">
     <input type="submit" value="Valider" />
 </form>
