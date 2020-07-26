00dd8338fdfcd2ff5a312e4bae04b3fa08beb059
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-07-26 16:12:14 +0200

    ajout commentManager.php

---
+++ Model/commentManager.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\Database;
+use DateTime;
+
+class CommentManager extends Database
+{ 
+
+    public function getComment($commentId) 
+    {   
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('SELECT id, postNumber, content, creationDate, updateDate FROM comment WHERE id = ?'); // formater la date dans la vue + table avec 5 champs
+        $req->execute(array($commentId));
+
+        return $this->hydrate($req->fetch());
+    }
+
+    public function getComments() 
+    {   
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('SELECT id, postNumber, content, creationDate, updateDate FROM comment'); // formater la date dans la vue + table avec 5 champs
+        $req->execute();
+        $result = $req->fetchAll();
+        //var_dump($result);
+
+        $comments = [];
+        foreach ($result as $comment) {
+            var_dump($comment);
+            $comments[] = $this->hydrate($comment);
+        }
+        
+        return $comments;
+    }
+
+    public function create($commentClean)
+    {var_dump($commentClean);
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('INSERT INTO comment(id, postNumber, content, creationDate, updateDate) VALUES (?, ?, NOW(), NOW())');
+        $req->execute($commentClean['content']);
+
+        return $bdd->lastInsertId();
+    }
+
+    public function modify($commentId)
+    {
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('UPDATE comment SET content = ?, updateDate = NOW() WHERE id = ?');
+        
+        return $req->execute(array(
+            $commentClean['content']= $_POST['content'], 
+            $commentId
+        ));
+
+    }
+
+    public function delete($commentId)
+    {
+        $bdd = $this->dbConnect();
+        $req = $bdd->prepare('DELETE FROM comment WHERE id = ?');
+        
+        return $req->execute(array($commentId));
+    }
+
+    public function hydrate($data)
+    {
+        /*var_dump($data);
+        var_dump(new DateTime());
+        var_dump(new DateTime($data['creationDate']));*/
+        $comment = new comment();
+        $comment
+            ->setId($data['id'])
+            ->setPostNumber($data['postNumber'])
+            ->setContent($data['content'])
+            ->setCreationDate(new DateTime($data['creationDate']))
+            ->setUpdateDate(new DateTime($data['updateDate']))
+        ;
+        //var_dump($comment);
+        return $comment;
+    }
+}
