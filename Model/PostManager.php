a3cc6d8a765013e0daa9d752f81ae3cc642b9d4c
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-07 12:56:48 +0200

    mise a jour des colonnes post

--- Model/PostManager.php
+++ Model/PostManager.php
@@ -11,7 +11,7 @@ class PostManager extends Database
     public function getPost($postId) 
     {   
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('SELECT id, title, content, creationDate, updateDate FROM post WHERE id = ?'); // formater la date dans la vue + table avec 5 champs
+        $req = $bdd->prepare('SELECT post_id, post_title, post_content, post_creation, updateDate FROM post WHERE post_id = ?'); // formater la date dans la vue + table avec 5 champs
         $req->execute(array($postId));
 
         return $this->hydrate($req->fetch());
@@ -20,7 +20,7 @@ class PostManager extends Database
     public function getPosts() 
     {   
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('SELECT id, title, content, creationDate, updateDate FROM post'); // formater la date dans la vue + table avec 5 champs
+        $req = $bdd->prepare('SELECT post_id, post_title, post_content, post_creation, updateDate FROM post'); // formater la date dans la vue + table avec 5 champs
         $req->execute();
         $result = $req->fetchAll();
         //var_dump($result);
@@ -37,7 +37,7 @@ class PostManager extends Database
     public function create($postClean)
     {var_dump($postClean);
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('INSERT INTO post(title, content, creationDate, updateDate) VALUES (?, ?, NOW(), NOW())');
+        $req = $bdd->prepare('INSERT INTO post(post_title, post_content, post_creation, updateDate) VALUES (?, ?, NOW(), NOW())');
         $req->execute(array($postClean['title'], $postClean['content']));
 
         return $bdd->lastInsertId();
@@ -46,7 +46,7 @@ class PostManager extends Database
     public function modify($postId)
     {
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('UPDATE post SET title = ?, content = ?, updateDate = NOW() WHERE id = ?');
+        $req = $bdd->prepare('UPDATE post SET post_title = ?, post_content = ?, updateDate = NOW() WHERE post_id = ?');
         
         return $req->execute(array(
             $postClean['title'] = $_POST['title'],
@@ -59,7 +59,7 @@ class PostManager extends Database
     public function delete($postId)
     {
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('DELETE FROM post WHERE id = ?');
+        $req = $bdd->prepare('DELETE FROM post WHERE post_id = ?');
         
         return $req->execute(array($postId));
     }
@@ -68,13 +68,13 @@ class PostManager extends Database
     {
         /*var_dump($data);
         var_dump(new DateTime());
-        var_dump(new DateTime($data['creationDate']));*/
+        var_dump(new DateTime($data['post_creation']));*/
         $post = new Post();
         $post
-            ->setId($data['id'])
-            ->setTitle($data['title']) 
-            ->setContent($data['content'])
-            ->setCreationDate(new DateTime($data['creationDate']))
+            ->setId($data['post_id'])
+            ->setTitle($data['post_title']) 
+            ->setContent($data['post_content'])
+            ->setCreationDate(new DateTime($data['post_creation']))
             ->setUpdateDate(new DateTime($data['updateDate']))
         ;
         //var_dump($post);
