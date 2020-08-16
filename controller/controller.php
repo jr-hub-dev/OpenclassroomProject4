ae42c77e2fbfdf579aa4ce136b996ca9521956c7
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-16 19:15:45 +0200

    ajout fonction pour last post avec erreurs

--- controller/controller.php
+++ controller/controller.php
@@ -4,17 +4,11 @@ namespace App\Controller;
 use App\Model\PostController;
 
 
-
 class Controller
 {
     public function home()
     {
         $template = 'home';
         include '../view/layout.php';  
-    }
-    public function displayLastPost()
-    {
-        $postController = new PostController();
-        $postController-> displayLast($_GET['id']);
     }
 }
