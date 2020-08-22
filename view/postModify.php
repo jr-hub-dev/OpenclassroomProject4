eb6a51635e8f62d39af5d106fa3583d5be792aad
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-23 00:07:43 +0200

    ajout script tiny

--- view/postModify.php
+++ view/postModify.php
@@ -1,10 +1,14 @@
+<?php 
+    foreach($errors as $error){
+        echo $error .'<br/>';
+    }
+    require_once('tiny.php');
+?>
 <form method='post'>
     <label for='newBilletTitle'>Titre du chapitre</label>
     <input type='text' name='title' id='newBilletTitle' value="<?php if (isset($post)) echo $post->getTitle(); ?>">
 
     <label for='newBilletTitle'>Contenu</label>
-    <textarea name='content' id='newBilletTitle'><?php if (isset($post)) echo $post->getContent(); ?></textarea>
+    <textarea name='content' id='postContent'><?php if (isset($post)) echo $post->getContent(); ?></textarea>
     <input type="submit" value="Valider" />
 </form>
-<script src="http://cloud.tinymce.com/stable/tinymce.min.js?apiKey=4m7auxcdz1rf5ukur3vjcxmehp4ct0sipvfcfaepgabstrjj"></script> 
-<script>tinymce.init({selector:'textarea#postContent'});</script> <!---faire un template-->
