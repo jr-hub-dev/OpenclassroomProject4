a8b35517a930f74edd986782d978d758237025b1
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-23 00:05:53 +0200

    ajout de la page login

---
+++ view/loginPage.php
@@ -0,0 +1,8 @@
+<?php 
+
+?>
+<form method="post" > 
+	<label id="login" name="login">Login : </label><input type="text" id="login" placeholder="login" required/><br />
+	<label name="password"> Mot de passe : </label><input type="text" id="password" placeholder="Mot de passe" required><br />
+	<input type="submit" name="submit" id="connexion" value="Se connecter">
+</form>
