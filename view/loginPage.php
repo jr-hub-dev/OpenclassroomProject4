09d750f71e2013128ecdc73e039bf37e95035dee
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-29 17:56:21 +0200

    correction de bug

--- view/loginPage.php
+++ view/loginPage.php
@@ -1,8 +1,13 @@
 <?php 
-
+    foreach($errors as $error){
+        echo $error .'<br/>';
+    }
 ?>
-<form method="post" > 
-	<label id="login" name="login">Login : </label><input type="text" id="login" placeholder="login" required/><br />
-	<label name="password"> Mot de passe : </label><input type="text" id="password" placeholder="Mot de passe" required><br />
-	<input type="submit" name="submit" id="connexion" value="Se connecter">
+<form method="post" >
+	<label for="newBilletTitle">Login</label>
+	<input id="userLogin" type="text" name="userLogin" >
+	<label for="userPassword"> Mot de passe :</label>
+	<input id="userPassword" type="password" name="userPassword">
+
+	<input type="submit" value="Se connecter">
 </form>
