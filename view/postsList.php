e3782afe80be067d79220250ecfc685b5659b107
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-30 16:33:35 +0200

    test session admin

--- view/postsList.php
+++ view/postsList.php
@@ -16,7 +16,7 @@ if(array_key_exists('userLogin', $_SESSION)){//a mettre devant la ligne concern√
                     <td><a href="index.php?objet=post&action=view&id=<?php echo $post->getId(); ?>" title="modifier <?php echo $post->getTitle(); ?> - <?php echo $post->getId(); ?>"><?php echo $post->getTitle();?></a></td>
                     <td><?php echo $post->getContent(); ?></td>
                     <td><?php echo $post->getId(); ?></td>
-                    <?php if($_SESSION['userLevel'] = 'admin');?><td><a href="index.php?objet=post&action=modify&id=<?php echo $post->getId(); ?>" title="modifier <?php echo $post->getTitle(); ?> - <?php echo $post->getId(); ?>">Modifier</a></td>
+                    <td><?php if($_SESSION['userLevel'] = 'admin'){ echo '<a href="index.php?objet=post&action=modify&id=<?php echo $post->getId(); ?>" title="modifier <?php echo $post->getTitle(); ?> - <?php echo $post->getId(); ?>">Modifier</a></td>';}?>
                     <td><a href="index.php?objet=post&action=delete&id=<?php echo $post->getId(); ?>" title="supprimer <?php echo $post->getTitle(); ?> - <?php echo $post->getId(); ?>">Supprimer</a></td>
                 </tr>
             <?php } ?>
