7daa0553fb6c04fd4f9eec726bf1521861452983
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-08-31 18:23:29 +0200

    ajout logout pour user

--- view/header.php
+++ view/header.php
@@ -8,6 +8,9 @@
 			<li><a href=>Contact</a></li>
 			<li><a href= 'index.php?objet=user&action=create'>S'enregistrer</a></li>
 			<a href= 'index.php?objet=user&action=login'>S'identifier</a>
+			<?php if(!empty($_SESSION)){ //verifier que quelque chose est et pas n'est pas 
+				echo '<a href="index.php?objet=user&action=logout">Logout</a>';
+			}?>
 		</ul>
 		
 	</nav>
